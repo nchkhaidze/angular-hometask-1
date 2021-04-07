22a6c5ac996b850f03a8b91c9682d3655e146e32
nchkhaidze <nikita.chkhaidze@gmail.com>
2021-04-07 20:08:09 +0300

    improved styling

--- src/app/pokemon/homepage/homepage.component.ts
+++ src/app/pokemon/homepage/homepage.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent {
+    tags = [
+        'Angular',
+        'AngularJS',
+        'Angular Material',
+        'React',
+        'Redux',
+        'Vue',
+        'JavaScript',
+        'TypeScript',
+        'RxJS',
+        'HTML',
+        'HTML5',
+        'SASS',
+        'SCSS',
+        'Node.js',
+        'HTTP',
+        'HTTPS',
+        'HTTP/2',
+        'Git',
+        'GitHub',
+        'GitLab',
+    ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    myControl = new FormControl();
 }
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import {HomepageComponent} from './homepage/homepage.component';
 import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
 import { PokemonListItemComponent } from '../pokemon-list-item/pokemon-list-item.component';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {ReactiveFormsModule} from "@angular/forms"
+
 
 
 @NgModule({
   declarations: [HomepageComponent, PokemonListComponent, PokemonListItemComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+   
   ],
   exports: [HomepageComponent]
 })
